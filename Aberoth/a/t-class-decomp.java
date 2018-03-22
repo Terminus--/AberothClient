34e6d76168f4406d32abd0dc4c92efb8485c60a6
Terminus-- <collin.pritchard@yahoo.com>
2018-03-22 19:05:06 -0400

    Create t-class-decomp.java

---
+++ Aberoth/a/t-class-decomp.java
@@ -0,0 +1,23 @@
+
+package a;
+
+public final class t implements u
+{
+    private String a;
+    
+    public t(final String s, final int n) {
+        this.a = f.a(s, n + 1);
+    }
+    
+    public final int a() {
+        return this.a.length() + 3;
+    }
+    
+    public final char b() {
+        return '\u0011';
+    }
+    
+    public final String a() {
+        return "\u0011" + f.a(this.a);
+    }
+}
