6535b82d77c1ca555d3dc7750da577fe739c198d
Terminus-- <collin.pritchard@yahoo.com>
2018-03-22 18:53:33 -0400

    Create p-class-decomp.java

---
+++ Aberoth/a/p-class-decomp.java
@@ -0,0 +1,39 @@
+
+
+package a;
+
+public final class p implements u
+{
+    private final int a;
+    private final int b;
+    private final int c;
+    private final int d;
+    private final char a;
+    
+    public p(final char a, final int c, final int d, final int a2, final int b) {
+        this.a = a;
+        this.a = a2;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+    
+    public p(final String s, final int n) {
+        this.a = s.charAt(n + 1);
+        this.c = s.charAt(n + 2);
+        this.d = s.charAt(n + 3);
+        this.a = f.a(s, n + 4);
+        this.b = f.a(s, n + 6);
+    }
+    
+    public final int a() {
+        return 8;
+    }
+    
+    public final char b() {
+        return '\u0013';
+    }
+    
+    public final String a() {
+        return "\u0013" + this.a + (char)this.c + (char)this.d + f.a(this.a) + f.a(this.b);
+    }
